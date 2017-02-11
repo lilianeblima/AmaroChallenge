0f8651b5388511f5953ef662c51f93493c757198
Liliane Bezerra Lima <liliane.b.lima@outlook.com>
2017-02-11 13:18:05 -0200

    Design CatalogView

---
+++ AmaroChallenge/CardCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CardCollectionViewCell.swift
+//  AmaroChallenge
+//
+//  Created by Liliane Bezerra Lima on 11/02/17.
+//  Copyright © 2017 Liliane Bezerra Lima. All rights reserved.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var image: UIImageView!
+    @IBOutlet var labelName: UILabel!
+    @IBOutlet var labelNormalPrice: UILabel!
+    @IBOutlet var labelFinalPrice: UILabel!
+    
+}
---
+++ AmaroChallenge/CatalogViewController.swift
@@ -0,0 +1,40 @@
+//
+//  CatalogViewController.swift
+//  AmaroChallenge
+//
+//  Created by Liliane Bezerra Lima on 11/02/17.
+//  Copyright © 2017 Liliane Bezerra Lima. All rights reserved.
+//
+
+import UIKit
+
+class CatalogViewController: UIViewController {
+
+    @IBOutlet var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
+
+// MARK: - Collection View
+extension CatalogViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellCard", for: indexPath) as? CardCollectionViewCell {
+            cell.labelName.text = "fff"
+            return cell
+        }
+        return UICollectionViewCell()
+    }
+    
+}
